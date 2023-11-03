08bd79f0cd8ba4dd635d676661ac7832e5657eef
codeForMobile <code.mobile@protonmail.com>
2023-11-03 16:19:15 -0600

    setup apollo and view data on ui

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from '@apollo/client'
+import Dashboard from './screens/Dashboard';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
 
 export default function App() {
   return (
+    <ApolloProvider client={client}>
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Dashboard/>
     </View>
+    </ApolloProvider>
   );
 }
 
---
+++ screens/Dashboard.tsx
@@ -0,0 +1,28 @@
+import { gql, useMutation } from '@apollo/client'
+import React, { useState } from 'react'
+import {Button, Text, TextInput, View} from 'react-native'
+
+const Dashboard: React.FC = () => {
+    const [symbol, setSymbol] = useState('')
+    const [getQuote, {data, loading, error}] = useMutation(gql`
+        mutation($symbol: String!){
+            quote(symbol: $symbol){
+                change
+                changePercent
+                companyName
+                peRatio
+                symbol
+                previousClose
+            }
+        }
+    `);
+    return (<View>
+                <TextInput value={symbol} onChangeText={setSymbol}/>
+                <Button onPress={() => getQuote({
+                    variables: {symbol}
+                })} title="Get Quote"/>
+                <Text>{JSON.stringify(error || data)}</Text>
+            </View>)
+}
+
+export default Dashboard
